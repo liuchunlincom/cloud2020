13ec258c50dbd40d083def03007048c5e7b54160
刘纯林 <liuchunlin@cennavi.com.cn>
2020-10-28 18:03:35 +0800

    common

--- cloud-api-commons/src/main/java/com/cennavi/cloudalibaba/commons/CommonResult.java
+++ cloud-api-commons/src/main/java/com/cennavi/cloudalibaba/commons/CommonResult.java
@@ -4,16 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class CommonResult<T> {
     private Integer code;
     private String message;
     private T data;
 
+    public CommonResult() {
+    }
+
     public CommonResult(Integer code, String message) {
         this(code, message, null);
+    }
+
+    public CommonResult(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
     }
 
     public Integer getCode() {
--- cloud-api-commons/src/main/java/com/cennavi/cloudalibaba/commons/Payment.java
+++ cloud-api-commons/src/main/java/com/cennavi/cloudalibaba/commons/Payment.java
@@ -6,10 +6,32 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Payment implements Serializable {
     private Long id;
     private String serial;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, String serial) {
+        this.id = id;
+        this.serial = serial;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
 }
